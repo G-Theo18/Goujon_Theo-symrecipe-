76a61bf125a54c8fb9a696bd41518a01eb482eb2
tg853309 <tgou58@gmail.com>
2026-05-21 14:52:41 +0200

    Partie 2 : CRUD des ingrédients - Formulaire pour créer un ingrédient Personnalisation du rendu et message ash

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -42,6 +42,11 @@ final class IngredientController extends AbstractController
             $manager->persist($ingredient);
             $manager->flush();
 
+            $this->addFlash(
+                'success',
+                'Vos changements ont été enregistrés !'
+            );
+
             return $this->redirectToRoute('app_ingredient');
         }
 
